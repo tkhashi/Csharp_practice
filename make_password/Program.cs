3abc002f4b6b8d40d5585033312edaa6b9f67bca
vagrant <pupodenews@gmail.com>
2021-01-03 13:40:02 +0000

    [start]random password

---
+++ make_password/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace make_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        private static string GetNumLetter(Random random){
+            return random.Next(10).ToString();
+        }
+    }
+}
