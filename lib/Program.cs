0ae7fd057ebf04c8dd31f2e486c4d937e0f234b5
vagrant <pupodenews@gmail.com>
2021-01-03 11:01:37 +0000

    [finish]method make_random_alphabet

--- lib/Program.cs
+++ lib/Program.cs
@@ -6,22 +6,23 @@ using System.Threading.Tasks;
 
 namespace test1 {
 	class Program {
-		static void Main(string[] args) {
-			int count = 14;
-			string password = MakePassword(count);
+		static void Main(string[] args){
+            int count = 10;
+            string password = MakeLowerAlphabet(count);
             Console.WriteLine(password);
         }
-		private static string MakePassword(int count) {
-			Random random = new Random();
-			string password = string.Empty;
-			for (int i=0; i < count; i++){
-				password += random.Next(10);
+        private static string MakeLowerAlphabet(int count)
+        {
+            Random random = new Random();
+            string password = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                int n = random.Next(97, 123);
+                char c = (char)n;
+                password += c.ToString();
             }
-			return password;
+            return password;
         }
-			//Console.WriteLine(password);
-			//string message = "C#の世界へようこそ";
-			//Console.WriteLine(message);
     }
 }
 
