0511affb0d53cf64589bb1450b50dce7b5ef46dc
vagrant <pupodenews@gmail.com>
2021-01-03 14:43:03 +0000

    [finish]random make_password_12

--- make_password/Program.cs
+++ make_password/Program.cs
@@ -8,11 +8,47 @@ namespace make_password
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+            int count = 12;
+            Random random = new Random();
+            string password = MakePassword(random, count);
+            Console.WriteLine(password);
+            Console.ReadKey();
+        }
+        private static string MakePassword(Random random, int count){
+            string password = string.Empty;
+            for(int i = 0; i < count; i++){
+                int n = i % 4;
+                if(n == 0){
+                    password += GetNumLetter(random);
+                }
+                if(n == 1){
+                    password += GetUpperLetter(random);
+                }
+                if(n == 2){
+                    password += GetLowerLetter(random);
+                }
+                if(n == 3){
+                    password += GetMarkLetter(random);
+                }
+            }
+            return password;
         }
         private static string GetNumLetter(Random random){
             return random.Next(10).ToString();
+        }
+        private static string GetLowerLetter(Random random){
+            char c = (char)random.Next(97, 123);
+            return c.ToString();
+        }
+        private static string GetUpperLetter(Random random){
+            char c = (char)random.Next(65, 91);
+            return c.ToString();
+        }
+        private static string GetMarkLetter(Random random){
+            string[] mark = {"!","#","$","%","&","@"};
+            int n = random.Next(mark.Length);
+            return mark[n];
         }
     }
 }
