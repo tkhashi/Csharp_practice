352f505cc1e5d7f29bb1c5d70782d80139d820af
vagrant <pupodenews@gmail.com>
2021-01-04 09:52:13 +0000

    [start]PasswordGenerator

---
+++ Password Creater/PasswordGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Creater
+{
+    public class PasswordGenerator
+    {
+        private string GetNumLetter(Random random)
+        {
+            return random.Next(10).ToString();
+        }
+    }
+}
