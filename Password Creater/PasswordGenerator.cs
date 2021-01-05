cf53aebe7657beef9908a662dd79d8fef0db7ac6
vagrant <pupodenews@gmail.com>
2021-01-05 10:05:13 +0000

    add abstract class

--- Password Creater/PasswordGenerator.cs
+++ Password Creater/PasswordGenerator.cs
@@ -11,34 +11,15 @@ namespace Password_Creater
         public PasswordGenerator(Random random) {
             this.random = random;
         }
-        public string MakePassword(int count)
+        public string MakePassword(int count, LetterFactory factory)
         {
-
             string password = string.Empty;
-            for(int i = 0 ; i<count; i++){
-                int n = i % 4;
-                if (n == 0) password += GetNumLetter();
-                if (n == 1) password += GetLowerLetter();
-                if (n == 2) password += GetUpperLetter();
-                if (n == 3) password += GetMarkLetter();
+            for (int i = 0; i < count; i++)
+            {
+                Letter letter = factory.Create(random,i);
+                password += letter.GetLetter();
             }
             return password;
-        }
-        private string GetNumLetter() {
-            return random.Next(10).ToString();
-        }
-        private string GetLowerLetter() {
-            char c = (char)random.Next(97, 123);
-            return c.ToString();
-        }
-        private string GetUpperLetter() {
-            char c = (char)random.Next(65, 91);
-            return c.ToString();
-        }
-        private string GetMarkLetter() {
-            string[] mark = { "!", "#", "$", "%", "&", "@" };
-            int n = random.Next(mark.Length);
-            return mark[n];
         }
     }
 }
--- Password Creater/Program.cs
+++ Password Creater/Program.cs
@@ -10,8 +10,9 @@ namespace Password_Creater
         static void Main(string[] args) {
             Random random = new Random();
             int count = 12;
+            LetterFactory factory = new AllLetterFactory();
             PasswordGenerator generator = new PasswordGenerator(random);
-            string password = generator.MakePassword(count);
+            string password = generator.MakePassword(count, factory);
             Console.WriteLine(password);
             Console.ReadKey();
         }
