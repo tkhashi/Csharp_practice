2803c868fa707afc5bebd7811b3184918ecc52e2
vagrant <pupodenews@gmail.com>
2021-01-05 15:36:25 +0000

    add event handler Click

--- PasswordCreatorUI/MainForm.Designer.cs
+++ PasswordCreatorUI/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace PasswordCreatorUI
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -29,13 +29,72 @@ namespace PasswordCreatorUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Execute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumOfLetters = new System.Windows.Forms.NumericUpDown();
+            this.Password = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NumOfLetters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Execute
+            // 
+            this.Execute.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.Execute.Location = new System.Drawing.Point(348, 76);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(274, 19);
+            this.Execute.TabIndex = 0;
+            this.Execute.Text = "パスワード生成";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文字数";
+            // 
+            // NumOfLetters
+            // 
+            this.NumOfLetters.Location = new System.Drawing.Point(251, 77);
+            this.NumOfLetters.Name = "NumOfLetters";
+            this.NumOfLetters.Size = new System.Drawing.Size(75, 19);
+            this.NumOfLetters.TabIndex = 2;
+            this.NumOfLetters.ValueChanged += new System.EventHandler(this.NumOfLetters_ValueChanged);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(163, 114);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(477, 19);
+            this.Password.TabIndex = 3;
+            this.Password.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.NumOfLetters);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Execute);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NumOfLetters)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown NumOfLetters;
+        private System.Windows.Forms.TextBox Password;
     }
 }
 
--- PasswordCreatorUI/MainForm.cs
+++ PasswordCreatorUI/MainForm.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace PasswordCreatorUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumOfLetters_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            Password.Text = NumOfLetters.Value.ToString();
         }
     }
 }
