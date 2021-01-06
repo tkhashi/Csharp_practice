8d410e14cd2f682b6f8c72aa3cba6ba28a494663
vagrant <pupodenews@gmail.com>
2021-01-06 08:49:10 +0000

    add datebinding

--- WPF_PasswordCreator/MainWindow.xaml.cs
+++ WPF_PasswordCreator/MainWindow.xaml.cs
@@ -29,5 +29,10 @@ namespace WPF_PasswordCreator
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ WPF_PasswordCreator/MainWindowVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Password_Creater;
+
+namespace WPF_PasswordCreator
+{
+    public class MainWindowVM
+    {
+        private ILetterFactory factory;
+        private bool isNonMark;
+        public bool IsNonMark
+        {
+            get{ return isNonMark; }
+            set {
+                isNonMark = value;
+                if (isNonMark) {
+                    factory = new NonMarkLetterFactory();
+                } else {
+                    factory = new AllLetterFactory();
+                }
+            }
+        }
+        private int numOfLetters;
+        public int NumOfLetters
+        {
+            get { return numOfLetters; }
+            set { numOfLetters = value; }
+        }
+        public MainWindowVM()
+        {
+            NumOfLetters = 20;
+            IsNonMark = false;
+        }
+    }
+}
