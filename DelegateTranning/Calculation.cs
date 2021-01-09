4164f23951e1d21456a9b809040c39d9f42e18e6
vagrant <pupodenews@gmail.com>
2021-01-09 09:47:19 +0000

    pattern of no_delegate

--- DelegateTranning/Calculation.cs
+++ DelegateTranning/Calculation.cs
@@ -8,16 +8,17 @@ namespace DelegateTranning
 {
     public class Calculation
     {
-        private delegate int Function(int n);
+        //private delegate int Function(int n);
         private int Power(int n)
         {
             return n * n;
         }
         public int Execute(int n)
         {
-            Function function = Power;
-            int ans = function(n);
-            return ans;
+            //Function function = Power;
+            //int ans = function(n);
+            //int ans = Power(n);
+            return Power(n);
         }
     }
 }
