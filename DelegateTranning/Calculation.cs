1bd557873353f781fd98d0e9e8602af8e96126b7
vagrant <pupodenews@gmail.com>
2021-01-09 09:31:35 +0000

    practice delegate

---
+++ DelegateTranning/Calculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTranning
+{
+    public class Calculation
+    {
+        private delegate int Function(int n);
+        private int Power(int n)
+        {
+            return n * n;
+        }
+        public int Execute(int n)
+        {
+            Function function = Power;
+            int ans = function(n);
+            return ans;
+        }
+    }
+}
